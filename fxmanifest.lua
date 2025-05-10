a74dce182142ef37a044603506f35abb32554418
Jim Shield <gothicjim@gmail.com>
2025-05-10 20:07:22 +0100

    Update fxmanifest.lua to new format
    Removes core loading in `fxmanifest.lua` in favour of more "verbose" file loader in jim_bridge v`2.0.08+`

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -12,25 +12,12 @@ shared_scripts {
 	'locales/*.lua',
 	'config.lua',
 
-    -- Required core scripts
-    '@ox_lib/init.lua',
-    '@ox_core/lib/init.lua',
-
-    '@es_extended/imports.lua',
-
-    '@qbx_core/modules/playerdata.lua',
-
-    --Jim Bridge
+    --Jim Bridge - https://github.com/jimathy/jim-bridge
     '@jim_bridge/starter.lua',
 
 	'shared/*.lua',
 }
 client_scripts {
-    '@PolyZone/client.lua',
-    '@PolyZone/BoxZone.lua',
-    '@PolyZone/EntityZone.lua',
-    '@PolyZone/CircleZone.lua',
-    '@PolyZone/ComboZone.lua',
     'client.lua'
 }
 
